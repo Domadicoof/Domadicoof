7e48e535f0128c72fa19533045c17c3765380ec5
Domadicoof <119791286+Domadicoof@users.noreply.github.com>
2023-02-28 14:10:46 +0700

    Create true.lua

---
+++ Nomal/true.lua
@@ -0,0 +1,4 @@
+
+getgenv().Key = "NORMALHUB_rV5Dt1"
+getgenv().DiscordID = "865458556786311218"
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/normalhubv1/Normal_Hub_MB/main/V1.lua" ), true))();
