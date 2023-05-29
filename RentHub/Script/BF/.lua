4a96254273f63921a62ecf6f9c7a4c4133a74c48
Domadicoof <119791286+Domadicoof@users.noreply.github.com>
2023-05-29 21:50:46 +0700

    Update .lua

--- RentHub/Script/BF/.lua
+++ RentHub/Script/BF/.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Domadicoof/Domadicoof/4f6a8a192dad661cf0b3c9588bcc76ce436c5449/Protected.lua.txt"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Domadicoof/Domadicoof/270f172ea0b880b96313983f25373e455131ac54/Rent%20Hub.lua"))()
